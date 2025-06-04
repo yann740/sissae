7df0ae11e2335828ba296bed1c617642228c1277
yann740 <elieyann556@gmail.com>
2025-06-04 09:03:39 +0000

    Update vite.config.js
    change vite

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,22 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/build/', // Chemin de base des assets en production
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-   build: {
-    outDir: 'public/build',
-    emptyOutDir: true,
-    manifest: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-      },
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+        manifest: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash].[ext]',
+            },
+        },
     },
-  },
 });
